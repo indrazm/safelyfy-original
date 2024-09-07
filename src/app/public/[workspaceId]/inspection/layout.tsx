9b92357b9695c513a5026be2672d1ab57f83b0db
imerprdn <mrprdnid@gmail.com>
2024-09-07 07:22:31 +0700

    remove public page

--- src/app/public/[workspaceId]/inspection/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { MainAppLayout } from "@/components/App/MainApp/MainAppLayout"
-
-export default function Layout({ children }: childrenProps) {
-    return <MainAppLayout>{children}</MainAppLayout>
-}
